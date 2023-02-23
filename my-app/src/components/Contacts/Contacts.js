f94bc6053b99fb4e6c7b182918a54711c1b7c693
janvaljean <kurtuldu@gmx.de>
2023-02-23 09:03:10 +0100

    form added

--- my-app/src/components/Contacts/Contacts.js
+++ my-app/src/components/Contacts/Contacts.js
@@ -1,13 +1,16 @@
 import List from "./List/List"
 import Form from "./Form/Form"
-
+import { useState } from "react"
 
 
 const Contacts = () => {
+  const [contacts, setContacts] = useState([])
+
+
   return (
     <div>
       <List/>
-      <Form/>
+      <Form addContact = {setContacts} contacts={contacts}/>
 
     </div>
   )
--- my-app/src/components/Contacts/Form/Form.js
+++ my-app/src/components/Contacts/Form/Form.js
@@ -1,25 +1,40 @@
 import { useState } from "react"
 
-const Form = () => {
-    const [form, setForm] = useState({fullname: "", phone_number : ""})
+const initialValue = {fullname: "", phone_number : ""}
+
+const Form = (addContact ,contacts) => {
+    const [form, setForm] = useState(initialValue)
     const onChangeInput = (e) => {
         setForm({...form, [e.target.name]:e.target.value})
         
     }
+    const onSubmit = (e) => {
+      e.preventDefault();
+      if (form.fullname === "" || form.phone_number === "") {
+        return false
+        //invalid input
+      }
+      addContact([...contacts, form])
+      setForm(initialValue)
+
+
+    }
+    // form has endpoint so page is rendered
 
   return (
-    <div>
-        <div><input name="fullname" placeholder="Fullname" onChange={onChangeInput}/>
+    <form onClick={onSubmit}>
+
+        <div><input name="fullname" placeholder="Fullname" onChange={onChangeInput} value={form.fullname}/>
         </div>
-        <div><input name="phone_number" placeholder="Phone Number" onChange={onChangeInput}/>
+        <div><input name="phone_number" placeholder="Phone Number" onChange={onChangeInput} value={form.phone_number}/>
         </div>
         <div>
-            <button>Add</button>
+            <button onClick={onSubmit}>Add</button>
         </div>
 
       
       
-    </div>
+    </form>
   )
 }
 
